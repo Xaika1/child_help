4c60f244bda71c842be17ca2700d729576eeb558
Xaika1 <tsvetkov_igor@internet.ru>
2026-09-12 14:02:43 +0300

    beta-2

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -1,23 +1,33 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import *
-
+from .models import Users  # Импортируем конкретно нашу модель Users
 
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(required=True, label="Email")
     
     class Meta:
         model = User  
         fields = ["username", "email", "password1", "password2"]
+        labels = {
+            "username": "Логин",
+            "password1": "Пароль",
+            "password2": "Подтверждение пароля",
+        }
     
     def save(self, commit=True):
+        # 1. Создаём стандартного пользователя Django (нужен для системы авторизации)
         user = super().save(commit=False) 
         user.email = self.cleaned_data['email']
         
         if commit:
             user.save()
-            Staff.objects.update_or_create(user=user)
+            
+            # 2. Создаём запись в нашей кастомной таблице Users (нужна для логики записей)
+            Users.objects.create(
+                login=user.username,
+                password='auto'  # Реальный пароль уже безопасно хранится в стандартной таблице User
+            )
         
         return user
